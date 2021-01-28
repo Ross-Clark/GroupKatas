8d9f93c2c6d55cbdb0523a29cb843055ce5f1d65
Tom Robinson <thomas.robinson10@nhs.net>
2021-01-28 14:34:16 +0000

    week 1 solutions

---
+++ week 0  - Introduction to Katas/p1.py
@@ -0,0 +1,6 @@
+def solve(p):
+    for n in p:
+        if p.count(n) == 1:
+            return n
+        else:
+            pass
---
+++ week 0  - Introduction to Katas/p2.py
@@ -0,0 +1,8 @@
+def solve(p):
+    for n in p:
+        if p.count(n) == 2:
+            return True
+        else:
+            pass
+
+    return False
